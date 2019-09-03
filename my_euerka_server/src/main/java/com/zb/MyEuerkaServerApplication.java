3502a440c86459bb1c99910256b9e36375dba7ba
zhangsanfeng <279694714@qq.com>
2019-09-03 10:03:03 +0800

    update zhangsang

--- my_euerka_server/src/main/java/com/zb/MyEuerkaServerApplication.java
+++ my_euerka_server/src/main/java/com/zb/MyEuerkaServerApplication.java
@@ -6,6 +6,7 @@ import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
 @EnableEurekaServer
+//张三丰
 public class MyEuerkaServerApplication {
 
     public static void main(String[] args) {
