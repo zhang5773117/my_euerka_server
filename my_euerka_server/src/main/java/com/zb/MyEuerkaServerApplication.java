a12a9ce1ed346bc0e7e39bcdd25a4228d8bee882
zhang5773117 <40231462+zhang5773117@users.noreply.github.com>
2019-09-03 10:06:00 +0800

    Update MyEuerkaServerApplication.java

--- my_euerka_server/src/main/java/com/zb/MyEuerkaServerApplication.java
+++ my_euerka_server/src/main/java/com/zb/MyEuerkaServerApplication.java
@@ -7,7 +7,7 @@ import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 @SpringBootApplication
 @EnableEurekaServer
 //张三丰
-//王玉鹏
+//王玉鹏123
 public class MyEuerkaServerApplication {
 
     public static void main(String[] args) {
